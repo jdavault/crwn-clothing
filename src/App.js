9fd8584eed784edbc5736aa533886dcb6d726965
Joe Davault <joe.davault@gmail.com>
2020-12-23 13:53:39 -0700

    updating our app with a shop page and shop preview and item components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
-import { Switch, Route, Link } from 'react-router-dom';
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>);
-}
-
-const TopicList = (props) => (
-  <div>
-  <br/>
-  <Link to="/">HOME</Link>
-  <br/>
- <h1>TOPIC LIST PAGE</h1>
- <br/>
- <Link to={`${props.match.url}/13`}>Topic 13</Link>
- <Link to={`${props.match.url}/14`}>Topic 14</Link>
- <Link to={`${props.match.url}/15`}>Topic 15</Link>
-
-  </div>
-)
-
-const TopicDetail = (props) => {
-return (
-  <div>
-    <h1>TOPIC DETAIL PAGE: {props.match.params.topicId} </h1>
-  </div>
-)}
+import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
@@ -36,9 +9,7 @@ function App() {
       <Switch>
       </Switch>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/hats" component={HatsPage} />
-      <Route exact path="/topics" component={TopicList} />
-      <Route path="/topics/:topicId" component={TopicDetail} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
